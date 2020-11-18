dfc83a22aad63984d041d0746af6395772cc0922
Hari Vignesh <74590303+stringtherapy@users.noreply.github.com>
2020-11-18 22:57:47 +0530

    Update text.php

--- text.php
+++ text.php
@@ -1,15 +1,14 @@
-check!
 <?php
 session_start();
 if(!isset($_SESSION['name']))
-  header("Location:index.php");
-?>
+  header("Location:index.php");  //redirecting to login page if user is not logged in
+?>                               //especially useful if login has passwords
 <html>
 <head>
 <title>Chat</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-<style>  
+
+<style>
     *{margin:0px;
     padding:0px;
     }
@@ -25,9 +24,8 @@ if(!isset($_SESSION['name']))
     border-radius: 20px;
     background-color:transparent;
     color:white;
-          text-shadow: 1px 1px 2px gray;
-           font-size: 16px;
-
+    text-shadow: 1px 1px 2px gray;
+    font-size: 16px;
     }
      
      #nav li{
@@ -48,7 +46,6 @@ if(!isset($_SESSION['name']))
 
     input[type=submit] {
     border:none;
-    //background-color: #47d147;
     color:black;
     padding:10px;
     border-radius:25px;
@@ -62,100 +59,74 @@ if(!isset($_SESSION['name']))
 
 </style>
 
-<script>
-if ( window.history.replaceState ) {
+<script>                                                              //three lines to prevent form resubmission on reload
+if ( window.history.replaceState ) {                                  //javascript code source:www.webtrickshome.com
   window.history.replaceState( null, null, window.location.href );
 }
 </script>
 
 </head>
-<body onload="scrollWin()"> 
-<?php //include('window.php'); ?>  
+<body onload="scrollWin()">                                           //calling a function to loads window.php file
 
 <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
 <script type="text/javascript">   
-    var auto_refresh = setInterval(
+    var auto_refresh = setInterval(                                   //window.php is refreshed to display continous data.
     function ()
     {$('#Status').load('window.php');
     },1000); 
 </script> 
   
-<script type="text/javascript">     
+<script type="text/javascript">                                       //script to make window.php scroll down to bottom on load
     function scrollWin(){
      document.querySelector("#Status").scrollTop = document.querySelector("#Status").scrollHeight;
     }; 
 </script>  
   
 
-<div id="Status"  onload="scrollWin()"><br><br>
-<?php include('window.php'); ?><img src="load.gif" width="25px" height="25px" style="margin-left:20%;">
+<div id="Status"  onload="scrollWin()"><br><br>                        //the element where scroll function is applied.
+<?php include('window.php'); ?><img src="load.gif" width="25px" height="25px" style="margin-left:20%;"> //a little animation
 </div>
 
  
-<form action="textpro.php" method="post">
-<input type="text" name="message" value="" size="35" autocomplete="off">
-<input type="hidden" value="<?php echo $rand?>" name="check">
+<form action="text.php" method="post">                                     //this is where text.php starts.
+<input type="text" name="message" value="" size="35" autocomplete="off">   //getting message from user
 <input type="submit" value="send">
 </form>
 
 
 
 <?php
-$conn=mysqli_connect("sql307.epizy.com","epiz_27163442","migRiVCK3Bs","epiz_27163442_saga");
-date_default_timezone_set("Asia/Kolkata");   
+$conn=mysqli_connect("host","user","password","db");                      //establishing connection
+date_default_timezone_set("Asia/Kolkata");                                //defining time zone
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
-	if(isset($_POST['message']) AND !empty($_POST['message'])){
+	if(isset($_POST['message']) AND !empty($_POST['message'])){           //on receiving data from user
 
-    $name=$_SESSION['name'];    
-	$message=$_POST['message']; 
+    $name=$_SESSION['name'];
+	$message=$_POST['message'];                                           //storing name, message, date
 	$time=date("l h:i:sa");
 
-    $list=file_get_contents("profan.txt");
-    $exem = array("whole", "will", "other","mother","long","fell","best","face","star","ones","horn","chin","lock","full","test"); 
-	$exem2 = array("sex","tit","cum","ass","fux","god");
-    /*if(strripos($list,$_POST['message']) !== false){
-        echo "<font color=white>language!</font>";
-    }
-    else{ */
-
-    $me=explode(" ",$message);
+    $list=file_get_contents("profan.txt");                                //basic profanity check
+    $me=explode(" ",$message);                                            //for more details check my repo: bleepblop
     $length = count($me);
 	$censored='';
-    //core filtering
     for ($i = 0; $i < $length; $i++)
-		
 		{
-		    $melow[$i] = strtolower($me[$i]);	
-            $me2[$i] = preg_replace('/[^A-Za-z0-9\-]/', '', $melow[$i]);
-			if(strlen($me2[$i]) >= 4){									
-			$me3[$i] = "/$me2[$i]/";
-			if(preg_match($me3[$i], $list)==1){                       
-			if(!in_array($me2[$i], $exem)){											
-			$me[$i]="<strike><d style = opacity:20%>".$me[$i]."</d></strike>"; 
-				}
-				}
-				} else if(in_array($me2[$i], $exem2)){
-					$me[$i]="<strike><d style = opacity:20%>".$me[$i]."</d></strike>"; 
-				}	
-		$censored .= "$me[$i]"." ";
+        $me2[$i] = preg_replace('/[^A-Za-z0-9\-]/', '', $me[$i]);
+        if(strlen($me2[$i]) >= 4){
+        $me3[$i] = "/$me2[$i]/i";
+        if(preg_match($me3[$i], $list)==1){
+        if(!in_array($me2[$i], $exem)){
+        $me[$i] = str_repeat("*", strlen($me[$i]));
+        }
+        }
+        }
+		$censored .= "$me[$i]"." ";                                      //getting censored data to store in database
 		}
-//message is now censored
 	
 	mysqli_query($conn,"INSERT into chathistory(name,message,time) VALUES('$name','$censored','$time')");
-  // echo "<script>window.location.href='textpro.php'</script>";
 	}
-    
 }
 ?>
-
-<?php /*
-if(isset($_SESSION['scroll']))
-{
-   echo "<script>window.location.href='textpro.php'</script>";  
-   unset($_SESSION['scroll']);
-} */
-?>
-
 </body>
 </html>
