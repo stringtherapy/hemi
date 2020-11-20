08bc39afe18da6cc6178ea85109234aa67967807
night fury <74590303+stringtherapy@users.noreply.github.com>
2020-11-20 23:55:27 +0530

    Define mysql refresh rate

--- user/refresh.js
+++ user/refresh.js
@@ -2,5 +2,4 @@
     function () {
     $('#Status').load('window.php');
     },3000); 
-
-    //now it is set to 3seconds. You can change it to any value as you wish. but remember faster refresh rate takes more load from server
+    // it is now set to 3seconds. You can change it to any value as you wish. but remember faster refresh rate takes more load from server.
