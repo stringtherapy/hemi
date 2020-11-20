84c67f6c3c081d1b9084fb5d6d60b08eaaaf98aa
night fury <74590303+stringtherapy@users.noreply.github.com>
2020-11-20 17:58:06 +0530

    Update conn.php

--- user/conn.php
+++ user/conn.php
@@ -1,7 +1,12 @@
-<?
-$conn=mysqli_connect("localhost","root","","base");
+<?php
+//enter the four strings to establish connection with your database
+$conn=mysqli_connect("hostname","username","password","databasename");
+
 if (!$conn)
 {
 	die("not connected".mysqli_error($conn));
 }
+
+// define any timezone you want. check php manual for the exact names allowed. //https://www.php.net/manual/en/timezones.php
+date_default_timezone_set("Asia/Kolkata");
 ?>
