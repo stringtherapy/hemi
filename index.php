04874ac306aa4e4115826bf0c7f01e538e5f0332
night fury <74590303+stringtherapy@users.noreply.github.com>
2020-11-20 17:17:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,27 +11,20 @@
 </form>
 
 <?php
-
-include '/user/conn.php';
-
+include('user/conn.php');
 if($_SERVER['REQUEST_METHOD'] == "POST")
 {
     if(isset($_POST['name']) AND !empty($_POST['name'])){
-        
 	$name = mysqli_real_escape_string($conn,$_POST['name']);
-
 	$message = $name;
-
-	include '/extra/filter.php'; 
-
+	include('extra/profanitycheck/filter.php'); 
 	if ($check == 0){
-	session_start();
-	$_SESSION['name']=$name;
-    $_SESSION['rand']=rand(1,25);
-  	mysqli_query($conn,"INSERT INTO friends(name)VALUES('$name')");
-	header('Location:text.php');
+		session_start();
+		$_SESSION['name']=$name;
+    		$_SESSION['rand']=rand(1,25);
+  		mysqli_query($conn,"INSERT INTO friends(name)VALUES('$name')");
+		header('Location:text.php');
 	} else echo 'please try again';
-
     } else echo 'please enter a name'; 
 }
 ?>
