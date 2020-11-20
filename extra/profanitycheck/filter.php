3e27905bbe01478159ec33531851befb44e7f2bf
night fury <74590303+stringtherapy@users.noreply.github.com>
2020-11-20 17:20:23 +0530

    Add files via upload

---
+++ extra/profanitycheck/filter.php
@@ -0,0 +1,33 @@
+<?php 
+
+	$list1 = file_get_contents("profan.txt");         
+	$list2 = file_get_contents("three.txt");	 
+    $exem2 = explode("\n",$newlist);		  
+    
+//$message is the input from any other phps
+	$me=explode(" ",$message);				  
+    $length = count($me);				
+	$censored='';					 
+	$check=0;
+	for ($i = 0; $i < $length; $i++)					 	     
+	{
+	$me2[$i] = preg_replace('/[^A-Za-z0-9\-]/', '', $me[$i]);	 	            
+	    if(strlen($me2[$i]) >= 4){	                                                     			
+	    	$me3[$i] = "/$me2[$i]/";                                                       
+	    		if(preg_match($me3[$i], $list)==1){                              	    
+            			if(!in_array($me2[$i], $exem)){				 	     						
+	    					$me[$i]="<strike><d style = opacity:20%>".$me[$i]."</d></strike>";
+	    				    //$me[$i] = str_repeat("*", strlen($me[$i])); 
+	    				    $check .= $check++;     
+	    				}
+	    			}
+	    } else if(in_array($me2[$i], $exem2)){                                           
+	    		  $me[$i]="<strike><d style = opacity:20%>".$me[$i]."</d></strike>";             
+	    		  $censored .= "$me[$i]"." ";
+	    		  $check .= $check++;                                                      
+	    }
+	    
+	    //echo "<div id=output>$censored</div>";                                          
+	}	
+    	$exem = array("whole", "will", "other","mother","long","fell","best","face","star","ones","horn","chin","lock","full","test","phone","head"); 
+?>
