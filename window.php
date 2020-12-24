1e29f0c3f5c9f9dedea476e96a26a402fb7a5871
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2020-12-24 15:05:30 +0530

    Update window.php

--- window.php
+++ window.php
@@ -69,13 +69,13 @@ if(!isset($_SESSION['count'])) {
         $check = $_SESSION['count'];
 }
 
-     if($total > $check ) {                                                              		//generating notification 
-        $check = $_SESSION['count'] = $total;								//potential area to refresh single thread rather than whole table 
+     if($total > $check ) {                                                              		//detected a new entry in table 
+        $check = $_SESSION['count'] = $total;								
         $recent = mysqli_query($conn,"SELECT * FROM chathistory ORDER BY ID DESC LIMIT 1"); 
         $check=mysqli_fetch_array($recent);
         	if($_SESSION['name']==$check["name"]){                                             
             	//echo"<alert>message sent!</alert>";
-        	} else echo "<alert>new message from <i>$check[name]</i> &#8595;</alert><br>";
+        	} else echo "<alert>new message from <i>$check[name]</i> &#8595;</alert><br>";		// generating notification 
 }
 ?>
 </html>
