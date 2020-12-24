c01d8b923c9580bb6adf29144d0430d57e737ed1
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2020-12-24 18:43:25 +0530

    Update logincheck.php

--- extra/logincheck.php
+++ extra/logincheck.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['name']))
+if(!isset($_SESSION['name'])){
 	header("Location:index.php");
 }
 //if user tried to access the chat window without logging in, the user is redirected to index page
