e71775ad8ca0b57db5280b55f289d08ef3c76b6d
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2020-12-25 22:26:28 +0530

    Create check_server_type.php

---
+++ extra/check_server_type.php
@@ -0,0 +1,7 @@
+<?php
+if(!isset($_SESSION['server_name'])){
+	$chathistory = "chathistory"; 			     //default public server
+	} else {
+	$chathistory = $_SESSION['server_name']; //custom private server
+	}
+?>
