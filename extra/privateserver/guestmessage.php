74b0f8f294a3291bebcef677f918395f16307ef5
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2020-12-28 15:12:54 +0530

    Update guestmessage.php

--- extra/privateserver/guestmessage.php
+++ extra/privateserver/guestmessage.php
@@ -7,6 +7,7 @@ include('private_check.php');
 <title>Chat</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href ="../../css/private.css">
+ <script src="script/resub.js"></script>
 </head>
 
 
