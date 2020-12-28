027a3260508feed97c9a0405d3251b7374ec034c
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2020-12-28 10:48:27 +0530

    Update password_check.php

--- extra/privateserver/password_check.php
+++ extra/privateserver/password_check.php
@@ -1,15 +1,16 @@
 <?php
 $match = '';
 
-$search2="SELECT * FROM private_servers WHERE server_name='$_SESSION[server_name]'";
+$search2="SELECT * FROM private_servers WHERE lobby_key='$key'";
 $result2 = mysqli_query($conn, $search2);
 
-if(mysqli_num_rows($result2) !== 0) {
-
+if(mysqli_num_rows($result2) !== 1) {
+ echo "no table with name ". $key." found";
+} else {
 $row2 = mysqli_fetch_array($result2);
 if($row2['password']==$password){
 	$match = "YES";  
 	$ID = $row1['ID'];
+} //else echo "password dont match "; 
 } 
-}
 ?>
